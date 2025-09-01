5c391a43273c16278ea1781211d502e39f05bcd2
Ryan <ryanshaunsmith@gmail.com>
2025-09-01 20:17:24 +0100

    rename myLeads to myReads

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let myLeads = [];
+let myReads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
+  myReads.push(inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  localStorage.setItem("myReads", JSON.stringify(myReads));
   renderLeads();
 });
 
@@ -15,8 +15,8 @@ function renderLeads() {
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${myReads[i]}'>
+                    ${myReads[i]}
                 </a>
             </li>
         `;
