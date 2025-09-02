c7d71e1e7685eec04192bfe0376c4bc9f6bf0f01
Ryan <ryanshaunsmith@gmail.com>
2025-09-02 10:49:47 +0100

    add logic to delete reads

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 let myReads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 
 let readsFromLocalStorage = JSON.parse(localStorage.getItem("myReads"));
 
 if (readsFromLocalStorage) {
+  console.log(readsFromLocalStorage);
   myReads = readsFromLocalStorage;
   renderLeads();
 }
 
 inputBtn.addEventListener("click", function () {
+  console.log("clicked");
   myReads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myReads", JSON.stringify(myReads));
   renderLeads();
 });
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myReads = [];
+  renderLeads();
+});
+
 function renderLeads() {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < myReads.length; i++) {
     listItems += `
             <li>
                 <a target='_blank' href='${myReads[i]}'>
